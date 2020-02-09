79057160350c74c2b3bbcd2c602ef38f4db69e55
alex <apocthiel@gmail.com>
2020-02-09 00:16:34 -0800

    Incremented version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='tdirstat',
-    version='1.0.5',
+    version='1.1.0',
     description="Terminal-based directory statistics with a nice TUI and quick"
                 " actionable information",
     packages=find_namespace_packages(
